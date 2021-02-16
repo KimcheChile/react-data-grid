ac7d30f63f25f9630fc2232a17c5462e6d0b6343
jorge <jabecerra@uc.cl>
2021-02-16 14:41:36 -0300

    fix: remove ts-error comment

--- src/hooks/useCombinedRefs.ts
+++ src/hooks/useCombinedRefs.ts
@@ -1,13 +1,12 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
 export function useCombinedRefs<T>(...refs: readonly React.Ref<T>[]) {
   return useCallback(
     (handle: T | null) => {
       for (const ref of refs) {
-        if (typeof ref === 'function') {
+        if (typeof ref === "function") {
           ref(handle);
         } else if (ref !== null) {
-          // @ts-expect-error: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/31065
           ref.current = handle;
         }
       }
